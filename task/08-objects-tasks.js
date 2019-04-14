c3fec622ebbbfbc701f05e9a21e239d7fe87fd39
victoria <victoria-butrym@tut.by>
2019-04-14 19:09:00 +0300

    feat: add changes

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -22,7 +22,7 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(width, height) {
+function Rectangle(width, height) {    
     throw new Error('Not implemented');
 }
 
